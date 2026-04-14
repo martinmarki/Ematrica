2377a3357a6d476129a2a16613fc75d0b1aaace1
Martin Marki <martin.marki@icloud.com>
2026-04-14 11:32:46 +0200

    Initial Commit

---
+++ Ematrica/EmatricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmatricaApp.swift
+//  Ematrica
+//
+//  Created by Martin on 2026. 04. 14..
+//
+
+import SwiftUI
+
+@main
+struct EmatricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmatricaTests/EmatricaTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmatricaTests.swift
+//  EmatricaTests
+//
+//  Created by Martin on 2026. 04. 14..
+//
+
+import Testing
+@testable import Ematrica
+
+struct EmatricaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
